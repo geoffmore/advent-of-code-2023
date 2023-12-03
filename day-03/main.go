dff9408c3f46bb15cd50df1cce83e8a04b61c0d3
Jeff Moore <jeoffmore+git@gmail.com>
2023-12-03 15:07:43 -0500

    Day 3 part b working

--- day-03/main.go
+++ day-03/main.go
@@ -10,9 +10,13 @@ import (
 )
 
 // const file = "03_sample.txt"
+
 const file = "03_input.txt"
 
-const pattern = "([[:digit:]]{1,})"
+const patternA = "([[:digit:]]{1,})"
+
+// https://github.com/bufbuild/protoc-gen-validate/issues/68
+const patternB = "[*]"
 
 // Point represents a cartesian coordinate
 type Point [2]int
@@ -52,6 +56,32 @@ func (f Foo) adjacency() []Point {
 			if !ContainsPoint(points, point) {
 				result = append(result, point)
 				found[point] = true
+			}
+		}
+	}
+	return result
+}
+
+func (f Foo) adjacencyFn(fn func(p Point) bool) []Point {
+	var points, result, adj []Point
+
+	for i := f.span[0]; i < f.span[1]; i++ {
+		points = append(points, Point{i, f.y})
+	}
+	// Lazy adjacency calculation that doesn't de-duplicate, which also has an effect of overlapping numbers
+	for _, point := range points {
+		adj = append(adj, point.adjacency()...)
+	}
+	// Compare all adjacent points and add to result if they are not part of points or exist in map
+	// https://stackoverflow.com/questions/66643946
+	found := make(map[Point]bool)
+	for _, point := range adj {
+		if _, ok := found[point]; !ok {
+			if fn(point) {
+				if !ContainsPoint(points, point) {
+					result = append(result, point)
+					found[point] = true
+				}
 			}
 		}
 	}
@@ -132,14 +162,54 @@ func p03a(re *regexp.Regexp, data [][]byte) int {
 	return total
 }
 
-func main() {
+func p03b(pattern string, data [][]byte) int {
+	var total int
 
 	re := regexp.MustCompile(pattern)
+
+	// Collect all numbers as []Point
+	m := make(map[Point][]int)
+	for i, line := range data {
+		// Get the relative indices of numbers
+		nums := re.FindAllIndex(line, -1)
+		for _, num := range nums {
+			// Gather adjacent points
+			adjacentPoints := Foo{
+				y:    i,
+				span: Range{num[0], num[1]},
+			}.adjacency()
+
+			// Iterate over adjacentPoints and return Points where they match string
+			filtered := Data(data).filterPoints(adjacentPoints, "*")
+
+			// Gather results by point
+			for _, point := range filtered {
+				n, err := strconv.Atoi(string(data[i][num[0]:num[1]]))
+				p(err)
+				if _, ok := m[point]; !ok {
+					m[point] = make([]int, 0)
+				}
+				m[point] = append(m[point], n)
+			}
+		}
+	}
+	for _, nums := range m {
+		if len(nums) == 2 {
+			total += nums[0] * nums[1]
+		}
+	}
+	return total
+}
+
+func main() {
+
+	re := regexp.MustCompile(patternA)
 
 	lines, err := Scan(file)
 	p(err)
 
-	fmt.Println(p03a(re, lines))
+	fmt.Println("A: ", p03a(re, lines))
+	fmt.Println("B: ", p03b(patternA, lines))
 }
 
 func p(err error) {
@@ -172,6 +242,35 @@ func UniqAdjacentChars(data [][]byte, points []Point) []string {
 				m[s] = true
 			}
 		}
+	}
+	return result
+}
+
+type Data [][]byte
+
+func (d Data) filterPoints(points []Point, s string) []Point {
+	var result []Point
+	// Contains found strings
+	m := make(map[string]bool)
+	l, w := len(d), len(d[0])
+	for _, point := range points {
+		if (point[0] < w && point[0] >= 0) && (point[1] < l && point[1] >= 0) {
+			c := string(d[point[1]][point[0]])
+			if _, ok := m[s]; !ok {
+				if c == s {
+					result = append(result, point)
+					m[s] = true
+				}
+			}
+		}
+		/*
+			if true {
+				v := string(d[point[1]][point[0]])
+				_ = v
+				//if d[point[1]][point[0]] == []byte(s) {
+				result = append(result, point)
+			}
+		*/
 	}
 	return result
 }
