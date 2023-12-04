706bb39f79d565767550b12811d2bacc7892e5f0
Jeff Moore <jeoffmore+git@gmail.com>
2023-12-04 17:40:08 -0500

    Added aoclib.Intersect as an intermediate of aoclib.ContainCount

--- aoclib/helpers.go
+++ aoclib/helpers.go
@@ -28,10 +28,9 @@ func B2i(b []byte) (int, error) {
 	return strconv.Atoi(string(b))
 }
 
-// ContainCount finds the number of elem in reference that exist in input and returns an int. Limited to comparable because of map generation
-func ContainCount[K comparable](input []K, ref []K) int {
-	// func ContainCount(input []int, reference []int) int {
-	var result int
+// Intersect finds the elem in reference that exist in input and returns that. Limited to comparable because of map generation
+func Intersect[K comparable](input []K, ref []K) []K {
+	var result []K
 
 	// Build a map of desired values
 	m := make(map[K]bool)
@@ -41,8 +40,13 @@ func ContainCount[K comparable](input []K, ref []K) int {
 	// Iterate over input and increment if a match is found
 	for _, v := range input {
 		if m[v] {
-			result++
+			result = append(result, v)
 		}
 	}
 	return result
+}
+
+// ContainCount finds the number of elem in reference that exist in input and returns an int. Limited to comparable because of map generation
+func ContainCount[K comparable](input []K, ref []K) int {
+	return len(Intersect(input, ref))
 }
